b1bbb5458ec749fec387acb2d14a336267fc385b
XandeEmperor <117127463+XandeEmperor@users.noreply.github.com>
2024-01-02 20:42:04 -0300

    Projeto110.py

---
+++ Projeto110.py
@@ -0,0 +1,52 @@
+import random
+response="P"
+while response=="P":
+    N=random.randint(1,6)
+    if N==1:
+        print("[-----]") 
+        print("[     ]")
+        print("[  0  ]")
+        print("[     ]") 
+        print("[-----]")   
+    
+    if N==2:
+        print("[-----]") 
+        print("[   0 ]")
+        print("[     ]")
+        print("[ 0   ]") 
+        print("[-----]")  
+         
+    if N==3: 
+        print("[-----]") 
+        print("[    0]")
+        print("[  0  ]")
+        print("[0    ]") 
+        print("[-----]")
+          
+    if N==4:
+        print("[-----]") 
+        print("[0   0]")
+        print("[     ]")
+        print("[0   0]") 
+        print("[-----]")  
+               
+    if N==5:
+        print("[-----]") 
+        print("[0   0]")
+        print("[  0  ]")
+        print("[0   0]") 
+        print("[-----]")  
+        
+    if N==6:    
+        print("[-----]") 
+        print("[0   0]")
+        print("[0   0]")
+        print("[0   0]") 
+        print("[-----]")   
+        
+        
+        
+        
+        
+response=input("Pressione P para jogar novamente e n para sair") 
+print("\n")            
